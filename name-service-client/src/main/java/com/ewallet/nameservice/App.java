749421baf155fe2b342a68d78c9ba58b12e58d3a
Chirani Rajapaksha <chirani.rajapaksha.97@gmail.com>
2025-12-21 12:05:48 +0530

    Initial commit

---
+++ name-service-client/src/main/java/com/ewallet/nameservice/App.java
@@ -0,0 +1,10 @@
+package com.ewallet.nameservice;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
