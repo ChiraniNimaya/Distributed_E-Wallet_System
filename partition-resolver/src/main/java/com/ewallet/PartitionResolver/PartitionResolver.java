7bb22be24c4c068c2f80db98b631626a47cdfe2c
Chirani Rajapaksha <chirani.rajapaksha.97@gmail.com>
2026-01-18 17:44:31 +0530

    Change of partition deciding logic

--- partition-resolver/src/main/java/com/ewallet/PartitionResolver/PartitionResolver.java
+++ partition-resolver/src/main/java/com/ewallet/PartitionResolver/PartitionResolver.java
@@ -9,11 +9,12 @@ public final class PartitionResolver {
     private PartitionResolver() {
     }
 
+    public static final int NUM_ACCOUNT_PER_PARTITION = 100;
+
     public static String resolve(String accountId, int partitionCount) {
         validate(accountId, partitionCount);
 
-        int hash = hashAccountId(accountId);
-        int partitionIndex = Math.floorMod(hash, partitionCount);
+        int partitionIndex = (Integer.parseInt(accountId) <= NUM_ACCOUNT_PER_PARTITION) ? 0 : 1;
 
         return "PARTITION_" + partitionIndex;
     }
@@ -29,21 +30,6 @@ public final class PartitionResolver {
 
         if (partitionCount <= 0) {
             throw new IllegalArgumentException("PartitionCount must be > 0");
-        }
-    }
-
-    private static int hashAccountId(String accountId) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] digest = md.digest(accountId.getBytes(StandardCharsets.UTF_8));
-
-            return ((digest[0] & 0xff) << 24)
-                    | ((digest[1] & 0xff) << 16)
-                    | ((digest[2] & 0xff) << 8)
-                    | (digest[3] & 0xff);
-
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("SHA-256 not available", e);
         }
     }
 }
